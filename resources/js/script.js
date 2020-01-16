2e177af97eb14cca1d00a63c2bbf2bb1244e2fca
Nithin Vedamuthu <nithinmike@gmail.com>
2020-01-16 16:43:33 -0500

    fixed overflow on nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,20 +1,30 @@
 $(document).ready(function(){
     var nav = $('.js--main-nav');
     var obtener_ancho = $(window).width();
+    var header_height = $("header").height();
 
     $('.js--nav-icon').click(function() {
         var heroText = $('.hero-text-box');
 
         if (nav.is(':hidden')){
-            nav.slideToggle(200);
             heroText.fadeOut(100);
+            nav.slideToggle(200, function(){
+                header_height = $("header").height();
+                var nav_height = nav.height();
+
+                if (header_height < nav_height) {
+                    $("header").animate({
+                        height: nav_height+150
+                    }, 200);
+                    //$("header").height(nav_height + 150);
+                }
+            });
         } else {
+            //$("header").height('');
             nav.slideToggle(100);
             heroText.fadeIn(200);
         }
 
     });
-
-
 
 });
