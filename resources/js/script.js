8ba974757b85272de1b37ad8a430909fe2793133
nithinmike <59974680+nithinmike@users.noreply.github.com>
2020-01-22 22:36:40 -0500

    Update script.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,10 +2,11 @@ function initialize() {
         var center = {lat: 34.052235, lng: -118.243683};
         var Forest = {
             info: '<h5>Forest Place</h5><br>721 S Forest Ave<br>Ann Arbor, MI 48104<br><a href="https://goo.gl/maps/L8ETMBt7cRA2">View Apartment</a><br><a href="https://goo.gl/maps/L8ETMBt7cRA2">Get Directions</a>'
+            name: 'Forest Place'
         };
         
         var locations = [
-            [Forest.info,   34.046438, -118.259653, 0]
+            [Forest.info,   34.046438, -118.259653, Forest.name]
         ];
         
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -17,12 +18,19 @@ function initialize() {
         marker = new google.maps.Marker({
         position: new google.maps.LatLng(locations[count][1], locations[count][2]),
         map: map,
-        title: locations[count][0]
+        title: locations[count][3]
         });
         
     google.maps.event.addListener(marker, 'mouseover', (function (marker, count) {
         return function () {
-            infowindow.setContent(locations[count][0]);
+            infowindow.setContent(locations[count][3]);
+            infowindow.open(map, marker);
+        }
+    })(marker, count));
+        
+    google.maps.event.addListener(marker, 'click', (function (marker, count) {
+        return function () {
+            infowindow.setContent(locations[count][3]);
             infowindow.open(map, marker);
         }
     })(marker, count));
