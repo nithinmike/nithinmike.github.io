686c7cccc6e53409c4e7628e10b1a53bc2652f6d
nithinmike <59974680+nithinmike@users.noreply.github.com>
2020-01-22 23:07:31 -0500

    Update script.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -17,7 +17,7 @@ function initialize() {
     ];
         
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 9,
+        zoom: 25,
         center: center
     });
     
