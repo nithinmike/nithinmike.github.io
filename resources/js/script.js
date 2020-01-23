d13e823e51c78c0d8523f58ab47d7a1c4aa74938
nithinmike <59974680+nithinmike@users.noreply.github.com>
2020-01-22 21:40:29 -0500

    trying again

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,57 +1,3 @@
-$(document).ready(
-    
-     
-    
-    function(){
-    var nav = $('.js--main-nav');
-    var header_height = $("header").height();
-    var nav_height = nav.height();
-
-    // Open/close mobile topnav
-    $('.js--nav-icon').click(function() {
-        var heroText = $('.hero-text-box');
-
-        if (nav.is(':hidden')){
-            heroText.fadeOut(100);
-            $('.js-contact-form').fadeOut(100);
-            $('.js-apt-section').css('visibility', 'hidden');
-            $(".apt-select").css('visibility', 'hidden');
-            nav.slideToggle(200, function(){
-
-                if ($("header").height() < (nav.height()+150)) {
-                    $("header").height(nav.height()+150);
-                }
-            });
-        } else {
-            nav.slideToggle(0);
-            heroText.fadeIn(200);
-            $('.js-contact-form').fadeIn(200);
-            $('.js-apt-section').css('visibility', 'visible');
-            $(".apt-select").css('visibility', 'visible');
-        }
-    });
-
-
-    // Expand header if phone rotates and topnav is open
-    $(window).on('resize', function(){
-
-        if ((header_height < (nav.height()+150)) && (!nav.is(':hidden'))) {
-            $("header").height(nav.height()+150);
-        }
-    });
-
-    $('.apt-select').change(function () {
-        $('.js-apt-div').hide();
-        $('.'+$(this).val()).fadeIn();
-    });
-
-    $('.js-properties-multiple').select2();
-    $( '.swipebox' ).swipebox();
-    $('.carousel').carousel();
-    
-    }
-);
-
 function initMap() {
         var center = {lat: 34.052235, lng: -118.243683};  
         var locations = [
@@ -92,3 +38,55 @@ function initMap() {
   }
         
 }
+
+$(document).ready(
+    function(){
+        var nav = $('.js--main-nav');
+        var header_height = $("header").height();
+        var nav_height = nav.height();
+
+        // Open/close mobile topnav
+        $('.js--nav-icon').click(function() {
+            var heroText = $('.hero-text-box');
+
+            if (nav.is(':hidden')){
+                heroText.fadeOut(100);
+                $('.js-contact-form').fadeOut(100);
+                $('.js-apt-section').css('visibility', 'hidden');
+                $(".apt-select").css('visibility', 'hidden');
+                nav.slideToggle(200, function(){
+
+                    if ($("header").height() < (nav.height()+150)) {
+                        $("header").height(nav.height()+150);
+                    }
+                });
+            } else {
+                nav.slideToggle(0);
+                heroText.fadeIn(200);
+                $('.js-contact-form').fadeIn(200);
+                $('.js-apt-section').css('visibility', 'visible');
+                $(".apt-select").css('visibility', 'visible');
+            }
+        });
+
+
+        // Expand header if phone rotates and topnav is open
+        $(window).on('resize', function(){
+
+            if ((header_height < (nav.height()+150)) && (!nav.is(':hidden'))) {
+                $("header").height(nav.height()+150);
+            }
+        });
+
+        $('.apt-select').change(function () {
+            $('.js-apt-div').hide();
+            $('.'+$(this).val()).fadeIn();
+        });
+
+        $('.js-properties-multiple').select2();
+        $( '.swipebox' ).swipebox();
+        $('.carousel').carousel();
+    
+    }
+);
+
